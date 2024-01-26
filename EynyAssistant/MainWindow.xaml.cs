caf670aabd76827d7b32508312d233b85063e083
shisukon0210 <edward870210@gmail.com>
2024-01-26 17:31:39 +0800

    加入專案檔案。

---
+++ EynyAssistant/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using AngleSharp;
+using AngleSharp.Dom;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace EynyAssistant
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        async void ParseEynyHtml(object sender, RoutedEventArgs e)
+        {
+            string chromeDriverPath = @"C:\Users\hhcor\OneDrive\桌面";
+            IWebDriver driver = new ChromeDriver(chromeDriverPath);
+            driver.Navigate().GoToUrl("https://www.eyny.com/forum-3691-1.html");
+            driver.Quit();
+        }
+    }
+}
---
+++ EynyAssistant/Model/Post.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Eynyassistant.Model
+{
+    public class Post:INotifyPropertyChanged
+    {
+        private string imageUrl;
+
+        public string ImageUrl
+        {
+            get { return imageUrl; }
+            set
+            {
+                if(ImageUrl != value)
+                {
+                    imageUrl = value;
+                    OnPropertyChange(nameof(imageUrl));
+                }
+            }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if(Title != value)
+                {
+                    title = value;
+                    OnPropertyChange(nameof(title));
+                }
+            }
+        }
+        
+        private string url;
+
+        public string Url
+        {
+            get { return url; }
+            set
+            {
+                if(url != value)
+                {
+                    url = value;
+                    OnPropertyChange(nameof(url));
+                }
+            }
+        }
+
+        private DateTime postTime;
+
+        public DateTime PostTime
+        {
+            get { return postTime; }
+            set
+            {
+                if(postTime != value)
+                {
+                    postTime = value;
+                    OnPropertyChange(nameof(postTime));
+                }
+            }
+        }
+
+        private DateTime replyTime;
+
+        public DateTime ReplyTime
+        {
+            get { return replyTime; }
+            set
+            {
+                if(replyTime != value)
+                {
+                    replyTime = value;
+                    OnPropertyChange(nameof(replyTime));
+                }
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
